2c2e87d3ac6776534c1c703ca6fae1d13d754d38
Karan <Kd8121827@gmail.com>
2021-09-24 14:51:11 +1000

    New environmental varible

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,11 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 import os
 
-
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-    app.config.from_pyfile('dev.cfg')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     Bootstrap(app)
 
