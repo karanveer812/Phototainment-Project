dcd6347d8b0187f0b95e3bd49072c234b129c9a0
Karan <Kd8121827@gmail.com>
2021-09-24 16:06:45 +1000

    new

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ import os
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL?sslmode=require")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     Bootstrap(app)
